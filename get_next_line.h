cf0c4ff2b1c4082ebc6b6dc6ea48de55b8b3da65
achazal <arthur.chazal@gmail.com>
2015-03-12 16:30:02 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achazal <achazal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/19 19:13:52 by achazal           #+#    #+#             */
+/*   Updated: 2015/02/23 05:44:03 by achazal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+
+# define BUFF_SIZE 1000
+
+int get_next_line(int const fd, char **line);
+
+#endif
